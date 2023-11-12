29b90b54258e598e5c06bdaad041257fee02ab68
silviacremascoli <silcremascoli@gmail.com>
2023-11-12 16:00:13 +0100

    create and import Card component

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Card() {
+    return (
+        <div>
+            <img src="" alt=""/>
+            <div>
+                <h2>{}</h2>
+                <p>{}</p>
+            </div>
+        </div>
+    )
+}
