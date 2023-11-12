7ab91c61e6d8d263a4249f14e04d8c7cbba40b9b
silviacremascoli <silcremascoli@gmail.com>
2023-11-12 16:54:19 +0100

    make call to PokemonAPI

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 export default function Card() {
-    return (
-        <div>
-            <img src="" alt=""/>
-            <div>
-                <h2>{}</h2>
-                <p>{}</p>
-            </div>
-        </div>
-    )
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=1021";
+    fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {console.log(data)});
+
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src="" alt="" />
+      <h2>{}</h2>
+      <p>{}</p>
+    </div>
+  );
 }
