837dc0f432d339c04e0ef1d66ad2fa0483517321
silviacremascoli <silcremascoli@gmail.com>
2023-11-13 17:26:32 +0100

    import axios and add loop to API call

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Card() {
-    const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=1021";
-    fetch(apiUrl)
-    .then((res) => res.json())
-    .then((data) => {console.log(data)});
+  const [pokemonData, setPokemonData] = useState("");
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=1021";
+  axios.get(apiUrl).then((response) => {
+    response.data.results.forEach((pokemon) => {
+      console.log(pokemon);
+      /* pokemon.forEach((singlePokemon) => {
+        console.log(singlePokemon.url);
+        /* axios.get(singlePokemon.url).then((response) => {
+          console.log(response.data);
+        });
+      });*/
+    });
+  });
 
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
