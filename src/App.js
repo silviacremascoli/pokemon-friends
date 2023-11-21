cc240e0d438c51185d7af0dfbae217ecd07e1c2a
silviacremascoli <silcremascoli@gmail.com>
2023-11-21 15:05:57 +0100

    add search input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
+import axios from "axios";
 
 export default function App() {
+  const [pokemon, setPokemon] = useState();
+  const [pokemonData, setPokemonData] = useState([]);
+
+  const searchPokemon = () => {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+    axios.get(apiUrl).then((response) => {
+      console.log(response);
+      setPokemonData({
+        name: response.data.name,
+        image: response.data,
+      });
+    });
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    searchPokemon();
+  };
+
+  const getPokemon = (event) => {
+    setPokemon(event.target.value);
+  };
+
   return (
     <div>
-      {Array(151)
+      {/* {Array(151)
         .fill(true)
         .map((_, i) => (
           <Card key={i} />
-        ))}
+        ))} */}
+      <input
+        type="search"
+        name="search"
+        placeholder="Search for a Pokémon..."
+        className="form-control"
+        autoFocus="on"
+        onChange={getPokemon}
+      />
+      <input
+        type="submit"
+        name="submit"
+        value="🔍"
+        className="btn btn-light w-100 search-button"
+      />
     </div>
   );
 }
